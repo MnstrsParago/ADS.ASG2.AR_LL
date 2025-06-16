f86ba696aec2a092f33b786559093563fd23a23d
MnstrsParago <240887@astanait.edu.kz>
2025-06-17 00:00:13 +0500

    Linked List
    Same there as Array List

--- src/linkedlist/MyLinkedList.java
+++ src/linkedlist/MyLinkedList.java
@@ -13,33 +13,193 @@ public class MyLinkedList<T> implements MyList<T> {
     private MyNode tail;
     private int size;
 
-    public MyLinkedList() {
-        head = null;
-        tail = null;
+    @Override
+    public void add(T item) {
+        addLast(item);
+    }
+
+    @Override
+    public void addFirst(T item) {
+        MyNode node = new MyNode(item);
+        if (head == null) {
+            head = tail = node;
+        } else {
+            node.next = head;
+            head.prev = node;
+            head = node;
+        }
+        size++;
+    }
+
+    @Override
+    public void addLast(T item) {
+        MyNode node = new MyNode(item);
+        if (tail == null) {
+            head = tail = node;
+        } else {
+            tail.next = node;
+            node.prev = tail;
+            tail = node;
+        }
+        size++;
+    }
+
+    @Override
+    public void add(int index, T item) {
+        checkIndexForAdd(index);
+        if (index == 0) {
+            addFirst(item);
+            return;
+        }
+        if (index == size) {
+            addLast(item);
+            return;
+        }
+        MyNode newNode = new MyNode(item);
+        MyNode current = getNode(index);
+        MyNode prev = current.prev;
+        prev.next = newNode;
+        newNode.prev = prev;
+        newNode.next = current;
+        current.prev = newNode;
+        size++;
+    }
+
+    @Override
+    public void set(int index, T item) {
+        checkIndex(index);
+        getNode(index).value = item;
+    }
+
+    @Override
+    public T get(int index) {
+        checkIndex(index);
+        return getNode(index).value;
+    }
+
+    @Override
+    public T getFirst() {
+        return get(0);
+    }
+
+    @Override
+    public T getLast() {
+        return get(size - 1);
+    }
+
+    @Override
+    public void remove(int index) {
+        checkIndex(index);
+        MyNode node = getNode(index);
+        if (node.prev != null) node.prev.next = node.next;
+        else head = node.next;
+        if (node.next != null) node.next.prev = node.prev;
+        else tail = node.prev;
+        size--;
+    }
+
+    @Override
+    public void removeFirst() {
+        remove(0);
+    }
+
+    @Override
+    public void removeLast() {
+        remove(size - 1);
+    }
+
+    @Override
+    public int indexOf(Object object) {
+        MyNode current = head;
+        for (int i = 0; i < size; i++) {
+            if (current.value.equals(object)) return i;
+            current = current.next;
+        }
+        return -1;
+    }
+
+    @Override
+    public int lastIndexOf(Object object) {
+        MyNode current = tail;
+        for (int i = size - 1; i >= 0; i--) {
+            if (current.value.equals(object)) return i;
+            current = current.prev;
+        }
+        return -1;
+    }
+
+    @Override
+    public boolean exists(Object object) {
+        return indexOf(object) != -1;
+    }
+
+    @Override
+    public void sort() {
+        Object[] array = toArray();
+        java.util.Arrays.sort(array);
+        clear();
+        for (Object obj : array) {
+            addLast((T) obj);
+        }
+    }
+
+    @Override
+    public Object[] toArray() {
+        Object[] arr = new Object[size];
+        MyNode current = head;
+        int i = 0;
+        while (current != null) {
+            arr[i++] = current.value;
+            current = current.next;
+        }
+        return arr;
+    }
+
+    @Override
+    public void clear() {
+        head = tail = null;
         size = 0;
     }
 
-    @Override public void add(T item) {}
-    @Override public void set(int index, T item) {}
-    @Override public void add(int index, T item) {}
-    @Override public void addFirst(T item) {}
-    @Override public void addLast(T item) {}
-    @Override public T get(int index) { return null; }
-    @Override public T getFirst() { return null; }
-    @Override public T getLast() { return null; }
-    @Override public void remove(int index) {}
-    @Override public void removeFirst() {}
-    @Override public void removeLast() {}
-    @Override public void sort() {}
-    @Override public int indexOf(Object object) { return -1; }
-    @Override public int lastIndexOf(Object object) { return -1; }
-    @Override public boolean exists(Object object) { return false; }
-    @Override public Object[] toArray() { return null; }
-    @Override public void clear() {}
-    @Override public int size() { return size; }
+    @Override
+    public int size() {
+        return size;
+    }
+
+    private MyNode getNode(int index) {
+        MyNode current;
+        if (index < size / 2) {
+            current = head;
+            for (int i = 0; i < index; i++) current = current.next;
+        } else {
+            current = tail;
+            for (int i = size - 1; i > index; i--) current = current.prev;
+        }
+        return current;
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size)
+            throw new IndexOutOfBoundsException("Index: " + index);
+    }
+
+    private void checkIndexForAdd(int index) {
+        if (index < 0 || index > size)
+            throw new IndexOutOfBoundsException("Add Index: " + index);
+    }
 
     @Override
     public java.util.Iterator<T> iterator() {
-        return null;
+        return new java.util.Iterator<T>() {
+            MyNode current = head;
+            public boolean hasNext() {
+                return current != null;
+            }
+            public T next() {
+                T val = current.value;
+                current = current.next;
+                return val;
+            }
+        };
     }
 }
