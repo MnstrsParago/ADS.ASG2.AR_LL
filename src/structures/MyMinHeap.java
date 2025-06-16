44024ffd5569f971eafa4ea7cd92700a6b57c0b9
MnstrsParago <240887@astanait.edu.kz>
2025-06-17 00:51:45 +0500

    Impl.-ed Min Heap
    Array List

--- src/structures/MyMinHeap.java
+++ src/structures/MyMinHeap.java
@@ -1,3 +1,58 @@
 public class MyMinHeap<T extends Comparable<T>> {
-    
+    private MyArrayList<T> heap = new MyArrayList<>();
+
+    public void insert(T item) {
+        heap.addLast(item);
+        heapifyUp(heap.size() - 1);
+    }
+
+    public T extractMin() {
+        if (isEmpty()) throw new IllegalStateException("Heap is empty");
+        T min = heap.get(0);
+        heap.set(0, heap.get(heap.size() - 1));
+        heap.removeLast();
+        heapifyDown(0);
+        return min;
+    }
+
+    public boolean isEmpty() {
+        return heap.size() == 0;
+    }
+
+    public int size() {
+        return heap.size();
+    }
+
+    private void heapifyUp(int index) {
+        while (index > 0) {
+            int parent = (index - 1) / 2;
+            if (heap.get(index).compareTo(heap.get(parent)) >= 0) break;
+            swap(index, parent);
+            index = parent;
+        }
+    }
+
+    private void heapifyDown(int index) {
+        int left, right, smallest;
+        while (index < heap.size()) {
+            left = 2 * index + 1;
+            right = 2 * index + 2;
+            smallest = index;
+
+            if (left < heap.size() && heap.get(left).compareTo(heap.get(smallest)) < 0)
+                smallest = left;
+            if (right < heap.size() && heap.get(right).compareTo(heap.get(smallest)) < 0)
+                smallest = right;
+            if (smallest == index) break;
+
+            swap(index, smallest);
+            index = smallest;
+        }
+    }
+
+    private void swap(int i, int j) {
+        T temp = heap.get(i);
+        heap.set(i, heap.get(j));
+        heap.set(j, temp);
+    }
 }
